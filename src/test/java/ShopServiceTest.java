21bcf988dbd95d1e1e7ae440b9c4fd061d3ef35b
siqichen <siqi1987@hotmail.com>
2024-02-12 21:30:56 +0100

    resolve test problem

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShopServiceTest {
 
     @Test
-    void addOrderTest() {
+    void addOrderTest() throws ProductNotFoundException {
         //GIVEN
         ShopService shopService = new ShopService();
         List<String> productsIds = List.of("1");
@@ -28,8 +28,40 @@ class ShopServiceTest {
         List<String> productsIds = List.of("1", "2");
 
         //WHEN
-        Order actual = shopService.addOrder(productsIds);
+        Order actual = null;
+try {
+    actual = shopService.addOrder(productsIds);
+} catch (ProductNotFoundException e) {
 
+}
+
+        //THEN
+        assertNull(actual);
+    }
+
+    @Test
+    void changeOrderStatusTest() throws ProductNotFoundException {
+        //GIVEN
+        ShopService shopService = new ShopService();
+        List<String> productsIds = List.of("1");
+        Order order = shopService.addOrder(productsIds);
+
+        //WHEN
+        List<Order> actual = shopService.getOrdersByStatus(OrderStatus.IN_DELIVERY);
+
+        //THEN
+        Order expected = new Order(order.id(), order.products(), OrderStatus.IN_DELIVERY);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void changeOrderStatusTest_whenInvalidOrderId_expectNull() {
+        //GIVEN
+        ShopService shopService = new ShopService();
+
+        //WHEN
+        List<Order> actual = null;
+        actual = shopService.getOrdersByStatus(OrderStatus.IN_DELIVERY);
         //THEN
         assertNull(actual);
     }
