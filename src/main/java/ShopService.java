65b226b03c8c545f4464cacff42ed4c43ba31221
siqichen <siqi1987@hotmail.com>
2024-02-12 19:48:11 +0100

    modify method with optional in another file

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -2,6 +2,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -12,12 +13,12 @@ public class ShopService {
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
+            Optional<Product> optionalProduct= productRepo.getProductById(productId);
+            if (optionalProduct.isEmpty()) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
             }
-            products.add(productToOrder);
+            products.add(optionalProduct.get());
         }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
