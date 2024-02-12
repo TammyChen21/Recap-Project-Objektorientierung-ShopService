7d3d8f0a0c3807061157d1a39fb9f183150d6ddd
siqichen <siqi1987@hotmail.com>
2024-02-12 20:59:54 +0100

    update method

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,5 +1,6 @@
 
 import lombok.Data;
+import lombok.With;
 
 import java.util.List;
 
@@ -8,6 +9,9 @@ public record Order(
         List<Product> products,
         OrderStatus status
 ) {
-
+@With
+    public Order withStatus(OrderStatus status) {
+        return new Order(this.id, this.products, this.status);
+    }
 }
 
