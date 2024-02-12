5322f1ad8ef07200d74bd921cacda386b7899c7c
siqichen <siqi1987@hotmail.com>
2024-02-12 15:04:06 +0100

    resolve build problem

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -2,11 +2,9 @@ import java.util.List;
 
 public record Order(
         String id,
-        List<Product> products,
-        OrderStatus status
+        List<Product> products
+       // OrderStatus status
 ) {
 
-  /*  public OrderStatus status() {
-        return status;
-    }*/
-}
+    }
+
