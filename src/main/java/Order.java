f542da7b42e711d04cff55ede666e88ff3da31ec
siqichen <siqi1987@hotmail.com>
2024-02-12 14:49:32 +0100

    add orderstatus

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -2,6 +2,11 @@ import java.util.List;
 
 public record Order(
         String id,
-        List<Product> products
+        List<Product> products,
+        OrderStatus status
 ) {
+
+  /*  public OrderStatus status() {
+        return status;
+    }*/
 }
